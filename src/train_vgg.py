3c75ba8c41e239084b19d337bdbe02b4784bf035
Denys Lazarenko <denis.lazarenko@gmail.com>
2021-01-19 15:48:45 +0100

    VGG16: extract main function code into a separate file

---
+++ src/train_vgg.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+
+from tools import get_data_loaders
+from transfer_learning import prepare_model, train_model, evaluate_model
+
+
+BATCH_SIZE = 20
+VALID_SPLIT = 0.2
+SEED = 42
+
+LR = 0.001
+N_EPOCHS = 2
+
+
+if __name__ == "__main__":
+
+    # Load data
+    loaders, classes = get_data_loaders(
+        batch_size=BATCH_SIZE,
+        valid_split=VALID_SPLIT,
+        seed=SEED)
+    train_loader, valid_loader, test_loader = loaders
+    print(f"{len(train_loader)} train batches of size {BATCH_SIZE}")
+    print(classes)
+
+    # Instantiate a pre-trained VGG16 neural network
+    vgg16 = models.vgg16(pretrained=True)
+    # Freeze "feature" (conv+pool) layers, train all FC layers
+    trainable_layers = vgg16.classifier
+    prepare_model(model=vgg16,
+                  trainable_layers=trainable_layers,
+                  n_outputs=len(classes))
+
+    # TODO remove tmp check
+    for layer in [vgg16.features, vgg16.classifier]:
+        print(layer)
+        for p in layer.parameters():
+            print(f"requires_grad: {p.requires_grad}")
+
+    # Train model
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(trainable_layers.parameters(), lr=LR)
+    train_model(vgg16, train_loader, criterion, optimizer, N_EPOCHS)
+
+    # Evaluate model
+    evaluate_model(vgg16, test_loader, classes)
--- src/transfer_learning.py
+++ src/transfer_learning.py
@@ -1,18 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torchvision import models
 
-from tools import get_data_loaders
-
-
-BATCH_SIZE = 20
-VALID_SPLIT = 0.2
-SEED = 42
-
-LR = 0.001
-N_EPOCHS = 2
 
 train_on_gpu = torch.cuda.is_available()
 
@@ -88,7 +77,9 @@ def train_model(model,
 
 def evaluate_model(model,
                    test_loader,
-                   classes):
+                   classes,
+                   criterion,
+                   batch_size):
     """
     Evaluate model's prediction quality.
     """
@@ -122,7 +113,7 @@ def evaluate_model(model,
             correct_tensor.numpy()) if not train_on_gpu else np.squeeze(
             correct_tensor.cpu().numpy())
         # calculate test accuracy for each object class
-        for i in range(BATCH_SIZE):
+        for i in range(batch_size):
             # TODO replace 'i' or vectorize the calculation (confusion matrix?)
             label = target.data[i]
             class_correct[label] += correct[i].item()
@@ -145,36 +136,3 @@ def evaluate_model(model,
         100. * np.sum(class_correct) / np.sum(class_total),
         np.sum(class_correct), np.sum(class_total)))
 
-
-if __name__ == "__main__":
-
-    # Load data
-    loaders, classes = get_data_loaders(
-        batch_size=BATCH_SIZE,
-        valid_split=VALID_SPLIT,
-        seed=SEED)
-    train_loader, valid_loader, test_loader = loaders
-    print(f"{len(train_loader)} train batches of size {BATCH_SIZE}")
-    print(classes)
-
-    # Instantiate a pre-trained VGG16 neural network
-    vgg16 = models.vgg16(pretrained=True)
-    # Freeze "feature" (conv+pool) layers, train all FC layers
-    trainable_layers = vgg16.classifier
-    prepare_model(model=vgg16,
-                  trainable_layers=trainable_layers,
-                  n_outputs=len(classes))
-
-    # TODO remove tmp check
-    for layer in [vgg16.features, vgg16.classifier]:
-        print(layer)
-        for p in layer.parameters():
-            print(f"requires_grad: {p.requires_grad}")
-
-    # Train model
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(trainable_layers.parameters(), lr=LR)
-    train_model(vgg16, train_loader, criterion, optimizer, N_EPOCHS)
-
-    # Evaluate model
-    evaluate_model(vgg16, test_loader, classes)
